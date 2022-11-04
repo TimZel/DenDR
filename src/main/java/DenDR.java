2aa0edc4b640ac4ae0ffce8e531491dbeebf990e
TimZel <zelyanin2010@yandex.ru>
2022-11-04 23:39:22 +0300

    C DR, Denis!

---
+++ src/main/java/DenDR.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class DenDR {
+        public static void main(String[] args) {
+            String[] wordListOne = {"Генеральный", "Уважаемый", "Достопочтенный", "Непревзойденный", "Августейший"};
+            String[] wordListTwo = {"Зять, ", "ДенГир, ", "Денис Анатольевич, ", "Отец моих племянников, ", "Фулстэк, ", "Мистер Д., "};
+            String[] wordListThree = {"С Юбилеем!", "С Праздником!", "С Днём Рождения!", "С Днюхой!", "С Юбилейным!",
+                    "С нетривиальной датой!", "С XXXX!", "С 40-м кольцом на срезе!"};
+            String[] wordListFour = {" Щасья, здаровья!", " Гламура и глянца!", " Бодрячком!", " Будь!", " Оторвись!",
+                    " Успеха, достижения всех целей!", " Улыбайся!", " Удивляйся!", " Радости!", " Тепла и чудачества!", " Вдохновения!",
+                    " Попугая на плече, капитан!", " Мёда и пчёл!" };
+
+            int oneLength = wordListOne.length;
+            int twoLength = wordListTwo.length;
+            int threeLength = wordListThree.length;
+            int fourLength = wordListFour.length;
+
+            int rand1 = (int) (Math.random() * oneLength); // приведения типа к инт, тк рэндом возвращает число между 0 и 1, умножаем на длину массива, чтобы выбор был между 0 и длиной массива
+            int rand2 = (int) (Math.random() * twoLength);
+            int rand3 = (int) (Math.random() * threeLength);
+            int rand4 = (int) (Math.random() * fourLength);
+
+            String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3] + wordListFour[rand4];
+
+            System.out.println("Цэ " + phrase);
+        }
+    }
+
+
